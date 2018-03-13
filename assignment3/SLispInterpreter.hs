884dd928ad9e509b659bd1ff63955b9470ade485
Pikby <jacobajensen@hotmail.ca>
2018-03-12 22:08:58 -0700

    Finished tokenizer, maybe, and started main driver

--- assignment3/SLispInterpreter.hs
+++ assignment3/SLispInterpreter.hs
@@ -1,3 +1,16 @@
 --Main program driver
 --Uses the functions from the other files to interpret SLisp
 --May be replaced as main driver by Evaluator.hs
+import Tokenizer
+import Parser
+import Data.Maybe
+
+import System.Environment
+import System.IO
+main = do
+    fileName <- getArgs
+    srcText <- readFile (head fileName)
+    -- Output from Tokenizer
+    print (tokenizeMain(['\n'] ++srcText))
+    -- Output from Parser
+    print (parseSExpression(fromMaybe [] (tokenizeMain(['\n'] ++srcText))))
--- assignment3/Tokenizer.hs
+++ assignment3/Tokenizer.hs
@@ -5,18 +5,23 @@
 --Ensure the whole file is read
 --Use elsewhere defined token type(s) for haskell representation
 
-import System.Environment
-import System.IO
+module Tokenizer where
+
+
 import Data.Maybe
 import Data.Char
+import Control.Exception
 import Types
-parseString :: [Char] -> Maybe (Token, [Char])
-parseMain :: [Char] -> Maybe [Token]
 
 
-parseAlphaNum :: [Char] -> [Char]
-parseSymbol :: [Char] -> [Char]
-parseNumeral :: [Char] -> [Char]
+
+tokenizeString :: [Char] -> Maybe ([Token], [Char])
+tokenizeMain :: [Char] -> Maybe [Token]
+
+
+tokenizeAlphaNum :: [Char] -> [Char]
+tokenizeSymbol :: [Char] -> [Char]
+tokenizeNumeral :: [Char] -> [Char]
 
 isSpeSymbol :: Char -> Bool
 isSpeSymbol(a)
@@ -24,46 +29,59 @@ isSpeSymbol(a)
   | a == '#' || a == '$' || a == '%' || a == '?' || a == ':' = True
   | otherwise = False
 
-parseSymbol [] = ""
-parseSymbol (a:rest)
-  | isSpeSymbol(a) = [a] ++ parseSymbol(rest)
+tokenizeSymbol [] = ""
+tokenizeSymbol (a:rest)
+  | isSpeSymbol(a) = [a] ++ tokenizeSymbol(rest)
   | otherwise   = ""
 
-parseNumeral [] = ""
-parseNumeral (a:rest)
-  | isDigit(a) = [a] ++ parseNumeral(rest)
+tokenizeNumeral [] = ""
+tokenizeNumeral (a:rest)
+  | isDigit(a) = [a] ++ tokenizeNumeral(rest)
   | otherwise = ""
 
-parseAlphaNum [] = ""
-parseAlphaNum (a:rest)
-  | isDigit(a) || isLetter(a) = [a] ++ parseAlphaNum(rest)
+tokenizeAlphaNum [] = ""
+tokenizeAlphaNum (a:b:rest)
+  | isDigit(a) || isLetter(a)  = [a] ++ tokenizeAlphaNum([b]++rest)
+  | a == '-'  && (isDigit(b) || isLetter(b)) = [a]++[b]++ tokenizeAlphaNum(rest)
   | otherwise = ""
 
-parseString []= Nothing
-parseString ('-':rest)
-  | isDigit(head rest)  = Just (NumToken (negate (read (parseNumeral(rest)) :: Int)), (drop (length (parseNumeral(rest))) rest))
-  | head rest == '-' && rest !! 1 == '>' = Just (Arrow, drop 2 rest)
-  | otherwise = Just (SpecialToken ("-" ++ (parseSymbol(rest))), (drop (length (parseSymbol(rest))) rest))
+tokenizeComment [] = ""
+tokenizeComment (a:rest)
+  | a == '\n' = ""
+  | otherwise = [a] ++ (tokenizeComment rest)
 
+tokenizeString [] = Nothing
+tokenizeString ('-':rest)
+  | isDigit(head rest) = Just ([NumToken (negate (read (tokenizeNumeral(rest)) :: Int))], (drop (length (tokenizeNumeral(rest))) rest))
+  | head rest == '-' && rest !! 1 == '>' = Just ([Arrow], drop 2 rest)
+  | otherwise = Just ([SpecialToken ("-" ++ (tokenizeSymbol(rest)))], (drop (length (tokenizeSymbol(rest))) rest))
 
-parseString (a:rest)
-  | a == ';' = Just (Comment rest, "")
-  | a == '(' = Just (LBrak,rest)
-  | a == ')' = Just (RBrak,rest)
-  | a == '[' = Just (LBrace,rest)
-  | a == ']' = Just (RBrace,rest)
-  | a == '{' = Just (LParen,rest)
-  | a == '}' = Just (RParen,rest)
-  | a == '=' = Just (Equal,rest)
-  | a == '"' = Just (Quote,rest)
-  | a == ':' = Just (Colon,rest)
-  | isSpeSymbol(a)  = Just (SpecialToken ([a]++(parseSymbol(rest))) , (drop (length (parseSymbol(rest))) rest))
-  | isDigit(a)      = Just (NumToken (read (parseNumeral([a] ++ rest)) :: Int), (drop (length (parseNumeral(rest))) rest))
-  | isLetter(a)     = Just (AlphaNumToken ([a]++(parseAlphaNum(rest))) , (drop (length (parseAlphaNum(rest))) rest))
-parseToken :: [Char] -> Maybe [Token]
-parseToken s = do
-  (tok1,rest) <- parseString(s)
-  return ([tok1] ++ (fromMaybe [] (parseToken(rest))))
+tokenizeString ('\n':rest)
+  | length rest < 3 = Just ([],rest)
+  | (head rest) == ';' && rest !! 1 == ';' && rest !! 2 == ';' && rest !! 3 /= '\n' = Just ([Comment (drop 3 (tokenizeComment(rest)))], (drop (length (tokenizeComment(rest))) rest))
+  | otherwise  = Just ([], rest)
 
-parseMain s = parseToken(s)
---  return ([tok1]++tok2
+tokenizeString (a:rest)
+  | a == ';' || a== ' ' = Just ([],rest)
+  | a == '(' = Just ([LBrak],rest)
+  | a == ')' = Just ([RBrak],rest)
+  | a == '[' = Just ([LBrace],rest)
+  | a == ']' = Just ([RBrace],rest)
+  | a == '{' = Just ([LParen],rest)
+  | a == '}' = Just ([RParen],rest)
+  | a == '=' = Just ([Equal],rest)
+  | a == '"' = Just ([Quote],rest)
+  | a == ':' = Just ([Colon],rest)
+  | isSpeSymbol(a)  = Just ([SpecialToken ([a]++(tokenizeSymbol(rest)))] , (drop (length (tokenizeSymbol(rest))) rest))
+  | isDigit(a)      = Just ([NumToken (read (tokenizeNumeral([a] ++ rest)) :: Int)], (drop (length (tokenizeNumeral(rest))) rest))
+  | isLetter(a)     = Just ([AlphaNumToken ([a]++(tokenizeAlphaNum(rest)))] , (drop (length (tokenizeAlphaNum(rest))) rest))
+  | otherwise       = error("urmum")
+
+tokenizeToken :: [Char] -> Maybe [Token]
+tokenizeToken s = do
+  (tok1,rest) <- tokenizeString(s)
+  return (tok1 ++ (fromMaybe [] (tokenizeToken(rest))))
+
+tokenizeMain s = tokenizeToken(s)
+
+--tokenizeFile :: [Char] -> Maybe [Token]
