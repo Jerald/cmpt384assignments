65761ec18ad9f3bfb4c09d5d4e27b2cefb0df1ea
Syou (Cloud) Kanou <qwerty0726@hotmail.com>
2018-04-05 00:54:41 -0700

    Hopefuly finished parser

--- assignment4/Parser.hs
+++ assignment4/Parser.hs
@@ -19,7 +19,7 @@ splitHypsAndGoals (fst:rest) (hyps,goals)
     | fst /= Turnstile               = splitHypsAndGoals rest (hyps++[fst], goals)
     | otherwise                      = (hyps, rest)
 
---Takes a stream of tokens and separates into individual propositional formulas
+--Takes a stream of tokens and separates into individual propositional formulaes
 splitCommas :: [Token] -> [Token] -> [[Token]] -> [[Token]]
 splitCommas (fst:rest) temp resTemp
     | fst /= Comma && rest /= [] = splitCommas rest (temp++[fst]) resTemp
@@ -27,9 +27,9 @@ splitCommas (fst:rest) temp resTemp
     | fst /= Comma && rest == [] = resTemp ++ [temp++[fst]]
     | fst == Comma && rest == [] = resTemp ++ [temp]
 
-parseTokens :: [Token] -> ProofConjecture
-parseTokens [] = Conjecture [] []
-parseTokens tokens = 
+makeConjecture :: [Token] -> ProofConjecture
+makeConjecture [] = Conjecture [] []
+makeConjecture tokens = 
     let splitted = splitHypsAndGoals tokens ([],[]) 
     in Conjecture (map parseMain (splitCommas (fst splitted) [] [])) (map parseMain (splitCommas (snd splitted) [] []))
 
@@ -37,38 +37,38 @@ parseMain :: [Token] -> PF
 parseMain tokens = 
     case parseStatement tokens of
         (res, []) -> res
-        _         -> error("Parse Fail")
+        _         -> error("Parse Fail: token stream is not empty")
 
 parseStatement :: [Token] -> (PF, [Token])
-parseStatement (Letter x:rest) = (Prop x, rest)
+parseAtom :: [Token] -> (PF, [Token])
+
 parseStatement (LBrack:rest) = 
     case parseStatement rest of
         (res, RBrack:even_more) -> (Group res, even_more)
-        _                       -> error("Parse Fail")
-parseStatement (Tilde:rest) =
+
+parseStatement (Tilde:LBrack:rest) = 
     case parseStatement rest of
-        (res, even_more) -> (Neg res, even_more)
---        _                -> error("Parse Fail")
+        (res, RBrack:even_more) -> (Neg (Group res), even_more)
+
 parseStatement tokens = 
-    case parseStatement tokens of
+    case parseAtom tokens of
         (res, Arrow:rest) ->
             case parseStatement(rest) of
                 (res1, even_more) -> (Imp res res1, even_more)
---                _                 -> error("Parse Fail")
 
         (res, Hat:rest)   ->
             case parseStatement(rest) of
                 (res1, even_more) -> (Conj res res1, even_more)
---                _                 -> error("Parse Fail")
 
         (res, Vee:rest)   ->
             case parseStatement(rest) of
                 (res1, even_more) -> (Disj res res1, even_more)
---                _                 -> error("Parse Fail")
 
         (res, Equal:rest) ->
             case parseStatement(rest) of
                 (res1, even_more) -> (Equiv res res1, even_more)
---                _                 -> error("Parse Fail")
 
-        _                 -> error("Parse Fail")
+        (res, rest)       -> (res, rest)
+
+parseAtom (Letter x:rest) = (Prop x, rest)
+parseAtom (Tilde:Letter x:rest) = (Neg(Prop x), rest)
